47df61ff312035c6dfe59aa8e2d9b6641295ecd6
taksenov <taksenov@gmail.com>
2014-10-15 12:50:16 +0600

    Fix for blankForm

--- blankform/views.py
+++ blankform/views.py
@@ -65,7 +65,6 @@ def comment(request):
 
     errors = []
     error = ''
-    request.session._get_or_create_session_key()
     my_session = request.session.session_key
 
     all_questions = connection.cursor()
